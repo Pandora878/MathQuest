8533da3d9cd4e93d2606ad9457c8957bd9628a11
Julia_Amabily <97228461+Pandora878@users.noreply.github.com>
2026-09-23 14:18:09 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.MATHQUEST_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA7t0vhYZmyuLEfN0yGOMARHGzusbaryu8",
+  authDomain: "mathquest-e046d.firebaseapp.com",
+  projectId: "mathquest-e046d",
+  storageBucket: "mathquest-e046d.firebasestorage.app",
+  messagingSenderId: "1090100159244",
+  appId: "1:1090100159244:web:6bea778c465dff06729f29",
+  measurementId: "G-RPF0BBW2MN"
+};
